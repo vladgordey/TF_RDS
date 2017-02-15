1a85741fdb0cda1cb311b1d59bb4e911cc7bf86d
vladgordey <vladgordey@gmail.com>
2017-02-14 20:25:53 -0500

    Update main_rds.tf

--- main_rds.tf
+++ main_rds.tf
@@ -9,9 +9,6 @@
 
 // Provider specific configs
 provider "aws" {
-    access_key = "${var.aws_access_key}"
-    secret_key = "${var.aws_secret_key}"
-    region = "${var.aws_region}"
 }
 
 resource "aws_db_instance" "main_rds_instance" {
